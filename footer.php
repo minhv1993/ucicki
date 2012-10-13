305f1286e13a497d93ec122f9d9d4a5245aecb34
Minhnhut Thanh Vo <locchoc_23510@yahoo.com>
2012-10-12 23:20:41 -0700

    designed the flashpage

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 <!-- the footer contains all the thing that all the pages have in common at the bottom, like the division and CKI international logos -->
 	<div id="footer">
-		This is the the footer
+		<div id="logos">
+			<a href="http://magickingdomdivision.blogspot.com/" target="_blank"><img src="images/mklogo.png" class="logo" /></a>
+			<a href="http://cnhcirclek.org/index.php?option=com_content&view=article&id=125:magic-kingdom-division" target="_blank"><img src="images/cnhlogo.png" class="logo"/></a>
+			<a href="http://www.circlek.org/home.aspx" target="_blank"><img src="images/ckilogo.gif" class="logo"/></a>
+		</div>
+		<div id="copyright">&copy; Copyright 2012-2013 | University of California, Irvine: Circle K | All rights reserved.<br/>
+			Peter Pan. &reg; and all characters and certain images are registered trademarks of the Walt Disney Inc.</div>
     </div>
     </div>
 </body>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php include('header.php'); ?>
 <div id="container">
-	
+	<div id="entersite"></div>
+	<div id="weeklyevents">
+		<div id="services"></div>
+		<div id="fellowship"></div>
+		<div id="usefullinks"></div>
+	</div>
 </div>
 <?php include('footer.php'); ?>
