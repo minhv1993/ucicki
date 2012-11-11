33afb22ae1878433c5623a94c9815a179902be9c
Minhnhut Thanh Vo <locchoc_23510@yahoo.com>
2012-11-10 22:56:00 -0800

    Iteration 1

--- header.php
+++ header.php
@@ -4,12 +4,68 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="styles/style.css" type="text/css" rel="stylesheet" />
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.0/jquery-ui.min.js" type="text/javascript"></script>
 <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
 <script src="js/jquery.hoverdir.js" type="text/javascript"></script>
 <script src="js/jquery.chilltip-packed.js" type="text/javascript"></script>
 <script src="js/jquery.onload.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(function() {
+		/**
+		* for each menu element, on mouseenter, 
+		* we enlarge the image, and show both sdt_active span and 
+		* sdt_wrap span. If the element has a sub menu (sdt_box),
+		* then we slide it - if the element is the last one in the menu
+		* we slide it to the left, otherwise to the right
+		*/
+        $('#sdt_menu > li').bind('mouseenter',function(){
+			var $elem = $(this);
+			$elem.find('img')
+				 .stop(true)
+				 .animate({
+					'width':'160px',
+					'height':'160px',
+					'left':'0px'
+				 },400,'easeOutBack')
+				 .andSelf()
+				 .find('.sdt_wrap')
+			     .stop(true)
+				 .animate({'top':'140px'},500,'easeOutBack')
+				 .andSelf()
+				 .find('.sdt_active')
+			     .stop(true)
+				 .animate({'height':'160px'},300,function(){
+				var $sub_menu = $elem.find('.sdt_box');
+				if($sub_menu.length){
+					var left = '160px';
+					if($elem.parent().children().length == $elem.index()+1)
+						left = '-160px';
+					$sub_menu.show().animate({'left':left},200);
+				}	
+			});
+		}).bind('mouseleave',function(){
+			var $elem = $(this);
+			var $sub_menu = $elem.find('.sdt_box');
+			if($sub_menu.length)
+				$sub_menu.hide().css('left','0px');
+			
+			$elem.find('.sdt_active')
+				 .stop(true)
+				 .animate({'height':'0px'},300)
+				 .andSelf().find('img')
+				 .stop(true)
+				 .animate({
+					'width':'0px',
+					'height':'0px',
+					'left':'85px'},400)
+				 .andSelf()
+				 .find('.sdt_wrap')
+				 .stop(true)
+				 .animate({'top':'25px'},500);
+		});
+    });
+</script>
 <title>Uci Circle K</title>
 </head>
 <body>
@@ -18,57 +74,54 @@
 	<div id="header">
 		<div id="logo"><a href="index.php"><img id="peteyLogo" src="images/peteyLogo.png" width="258px" height="165px" /></a></div>
 		<div id="tinker"></div><div id="chaseShadow"></div>
-		<div id="navbar">
-        <div class="contentnav">
+        <div id="navbar">
 			<ul id="sdt_menu" class="sdt_menu">
 				<li>
 					<a href="PeteyIndex.html">
-						<img src="images/2.jpg" alt="" style="width: 0px; height: 0px; left: 85px; ">
-						<span class="sdt_active" style="height: 0px; "></span>
-						<span class="sdt_wrap" style="top: 25px; ">
+                    	<img src="images/2.jpg" alt=""/>
+                        <span class="sdt_active"></span>
+						<span class="sdt_wrap">
 							<span class="sdt_link">Home</span>
 							<span class="sdt_descr">Return to Neverland</span>
 						</span>
 					</a>
-        </li>
+				</li>
 				<li>
 					<a href="#">
-						<img src="images/1.jpg" alt="" style="width: 0px; height: 0px; left: 85px; ">
-						<span class="sdt_active" style="height: 0px; "></span>
-						<span class="sdt_wrap" style="top: 25px; ">
+						<img src="images/1.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
 							<span class="sdt_link">About Us</span>
 							<span class="sdt_descr">Know More About CKI</span>
 						</span>
 					</a>
-                    <div class="sdt_box" style="display: none; left: 0px; ">
-               		  <a href="PeteyCKI.html">&nbsp; About CKI </a>
-					  <a href="Peteyhistory.html">&nbsp; History of CKI @ UCI </a>
-					  <a href="Peteypartnership.html">&nbsp; Partnership</a>		
-                      <a href="Peteyfaq.html">&nbsp; FAQ </a>				
-                  </div>
-					
-		    </li>
+                    <div class="sdt_box">
+                    		<a href="PeteyCKI.html">About CKI </a>
+							<a href="Peteyhistory.html">History of CKI @ UCI </a>
+							<a href="Peteypartnership.html">&nbsp Partnership</a>		
+                            <a href="Peteyfaq.html">FAQ </a>				
+                    </div>
+				</li>
 				<li>
 					<a href="Peteyevents.html">
-						<img src="images/3.jpg" alt="" style="width: 160px; height: 160px; left: 0px; ">
-						<span class="sdt_active" style="height: 160px; "></span>
-						<span class="sdt_wrap" style="top: 140px; ">
+						<img src="images/3.jpg" alt=""/>
+						<span class="sdt_active"></span>
+						<span class="sdt_wrap">
 							<span class="sdt_link">Events</span>
 							<span class="sdt_descr">Where Service Begins</span>
 						</span>
 					</a>
-                    
-                    <div class="sdt_box" style="display: block; left: 160px; ">
-                      <a href="Peteycalendar.html">&nbsp; Calendar</a>	
-					  <a href="Peteyevents.html">&nbsp; Weekly</a>
-					  <a href="Peteyevents.html">&nbsp; UCI Circle K </a>
-					  <a href="Peteyevents.html">&nbsp; Divisional </a>
-                      <a href="Peteyevents.html">&nbsp; District/Intl.</a>
+                    <div class="sdt_box">
+                    		<a href="Peteycalendar.html">Calendar</a>
+							<a href="Peteyevents.html">Weekly</a>
+							<a href="Peteyevents.html">UCI Circle K </a>
+							<a href="Peteyevents.html">Divisional </a>
+                            <a href="Peteyevents.html">District/Intl.</a>
 					</div>
 				</li>
 				<li>
 					<a href="#">
-						<img src="images/4.jpg" alt="">
+						<img src="images/4.jpg" alt=""/>
 						<span class="sdt_active"></span>
 						<span class="sdt_wrap">
 							<span class="sdt_link">Members</span>
@@ -76,39 +129,37 @@
 						</span>
 					</a>
                     <div class="sdt_box">
-                    		<a href="Peteyjoin.html">&nbsp; How to Join </a>
-							<a href="PeteyFamilies.html">&nbsp; Families </a>
-							<a href="PeteyCoBig12_13.html">&nbsp; Co-Bigs </a>
-							<a href="Peteymow.html">&nbsp; Member of the Week </a>
-                            <a href="Peteymom.html">&nbsp; Member of the Month</a>
-                            <a href="Peteymrp.html">&nbsp; Mem. Recognition Program</a>
+                    		<a href="Peteyjoin.html">How to Join </a>
+							<a href="PeteyFamilies.html">Families </a>
+							<a href="PeteyCoBig12_13.html">&nbsp Co-Bigs </a>
+							<a href="Peteymow.html">Member of the Week </a>
+                            <a href="Peteymom.html">Member of the Month</a>
+                            <a href="Peteymrp.html">Mem. Recognition Program</a>
 					</div>
 				</li>
 				<li>
 					<a href="#">
-						<img src="images/5.jpg" alt="">
+						<img src="images/5.jpg" alt=""/>
 						<span class="sdt_active"></span>
 						<span class="sdt_wrap">
 							<span class="sdt_link">Board/Committee</span>
 							<span class="sdt_descr">Know the Boards</span>
 						</span>
 					</a>
-                <div class="sdt_box">
-                    		<a href="PeteyBoard12_13.html">&nbsp; UCI CKI Board</a>
-							<a href="Peteycommittee.html">&nbsp; UCI CKI Committee </a>
-							<a href="Peteydistrictintl.html">&nbsp; District/Intl. CKI Board </a>
-							<a href="Peteyadvisors.html">&nbsp; CKI Advisors </a>                
-				  </div>    
-                    
-                    
-		    </li>
+                	<div class="sdt_box">
+                    		<a href="PeteyBoard12_13.html">&nbsp UCI CKI Board</a>
+							<a href="Peteycommittee.html">&nbsp UCI CKI Committee </a>
+							<a href="Peteydistrictintl.html">&nbsp District/Intl. CKI Board </a>
+							<a href="Peteyadvisors.html">&nbsp CKI Advisors </a>                
+					</div>
+				</li>
 				<li>
 					<a href="#">
-						<img src="images/6.jpg" alt="">
+						<img src="images/6.jpg" alt=""/>
 						<span class="sdt_active"></span>
 						<span class="sdt_wrap">
-							<span class="sdt_link">&nbsp;&nbsp;&nbsp;&nbsp; Resources</span>
-							<span class="sdt_descr">&nbsp; &nbsp; &nbsp; Forms,Agendas...</span>
+							<span class="sdt_link">&nbsp&nbsp&nbsp&nbsp Resources</span>
+							<span class="sdt_descr">&nbsp &nbsp &nbsp Forms,Agendas...</span>
 						</span>
 					</a>
 					<div class="sdt_box">
@@ -118,75 +169,14 @@
                         <a href="Peteycheers.html">Cheers</a>
 						<a href="Peteymedia.html">Multimedia</a>
 						<a href="Peteylinks.html">Links</a>
-                  </div>
+                   </div>
 				</li>
 			</ul>
 		</div>
-        <div>
+        <!--<div>
             <span class="reference">
                 <a href="index.html">back to event this week page!</a>
 				<a href="#">Never Growing out of Service!</a>
             </span>
-		</div>
-
-        <!-- The JavaScript -->
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-		<script type="text/javascript" src="jquery.easing.1.3.js"></script>
-        <script type="text/javascript">
-            $(function() {
-				/**
-				* for each menu element, on mouseenter, 
-				* we enlarge the image, and show both sdt_active span and 
-				* sdt_wrap span. If the element has a sub menu (sdt_box),
-				* then we slide it - if the element is the last one in the menu
-				* we slide it to the left, otherwise to the right
-				*/
-                $('#sdt_menu > li').bind('mouseenter',function(){
-					var $elem = $(this);
-					$elem.find('img')
-						 .stop(true)
-						 .animate({
-							'width':'160px',
-							'height':'160px',
-							'left':'0px'
-						 },400,'easeOutBack')
-						 .andSelf()
-						 .find('.sdt_wrap')
-					     .stop(true)
-						 .animate({'top':'140px'},500,'easeOutBack')
-						 .andSelf()
-						 .find('.sdt_active')
-					     .stop(true)
-						 .animate({'height':'160px'},300,function(){
-						var $sub_menu = $elem.find('.sdt_box');
-						if($sub_menu.length){
-							var left = '160px';
-							if($elem.parent().children().length == $elem.index()+1)
-								left = '-160px';
-							$sub_menu.show().animate({'left':left},200);
-						}	
-					});
-				}).bind('mouseleave',function(){
-					var $elem = $(this);
-					var $sub_menu = $elem.find('.sdt_box');
-					if($sub_menu.length)
-						$sub_menu.hide().css('left','0px');
-					
-					$elem.find('.sdt_active')
-						 .stop(true)
-						 .animate({'height':'0px'},300)
-						 .andSelf().find('img')
-						 .stop(true)
-						 .animate({
-							'width':'0px',
-							'height':'0px',
-							'left':'85px'},400)
-						 .andSelf()
-						 .find('.sdt_wrap')
-						 .stop(true)
-						 .animate({'top':'25px'},500);
-				});
-            });
-        </script>
-        </div>
+		</div>-->
     </div>
